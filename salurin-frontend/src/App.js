0609d2776e3cc803b55f0ba9c2461b51b62a6493
Zaka Noor <zakanoor@outlook.co.id>
2022-06-15 18:02:38 +0700

    SFP-18 in reviews

--- salurin-frontend/src/App.js
+++ salurin-frontend/src/App.js
@@ -1,18 +1,28 @@
 import logo from './logo.png';
 import './App.css';
 import Navbar from './Component/navbar';
-import Footer from './Component/Footer'
+import Footer from './Component/Footer';
+import Campaign from './Component/Campaign';
 
 function App() {
   return (
     <>
-    <Navbar/>
+    <Navbar />
     <div className="App">
       <header className="App-header">
         
       </header>
+
+      {/* Jumbotron */}
+
+      {/* Feature Component */}
+
+      {/* ------------------------------------- */}
+
+      {/* Campaign */}
+      <Campaign />
     </div>
-      <Footer />
+    <Footer />
     </>
   );
 }
