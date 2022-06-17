0fca0a7123d5b72b6f0e199d21247bbc32faa6ac
Zaka Noor <zakanoor@outlook.co.id>
2022-06-17 16:48:51 +0700

    SFP-18 docs: new file Home for landing page

---
+++ salurin-frontend/src/Component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import component
+
+function Home()
+{
+    return(
+        <>
+            <div className="App">
+                <header className="App-header">
+                
+                </header>
+            </div>
+        </>
+    )
+}
+
+export default Home
