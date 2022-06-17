254e4fb69b2db2abf92c7bbaa183ac60bd2879d6
Zaka Noor <zakanoor@outlook.co.id>
2022-06-17 16:53:43 +0700

    SFP-18 merge file from SFP-19-feature-component

---
+++ salurin-frontend/src/Component/Feature.js
@@ -0,0 +1,40 @@
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import EasyRegister_Logo from '../Assets/phone.png'
+import StartYourCampaign_Logo from '../Assets/campaign.png'
+import EpicStories_Logo from '../Assets/stories.png'
+import './Feature.css'
+
+function Feature() {
+    return(
+        <Container>
+            <Row className="d-flex justify-content-around" style={{margin:"10.75rem 0"}}>
+                <Col className="justify-content-center" style={{maxWidth:"22rem", maxHeight:"23rem"}}>
+                    <div style={{height:"14rem"}}>
+                        <Image src={EasyRegister_Logo}></Image>
+                    </div>
+                    <p className="feature-title">Easy Register</p>
+                    <p className="feature-text">Registrasi akun dengan mudah dan tanpa ribet</p>
+                </Col>
+                <Col className="justify-content-center" style={{maxWidth:"22rem", maxHeight:"23rem"}}>
+                    <div style={{height:"14rem"}}>
+                        <Image src={StartYourCampaign_Logo}></Image>
+                    </div>
+                    <p className="feature-title">Start Your Campaign</p>
+                    <p className="feature-text">Buat campaignmu terkait problem fasilitas pendidikan yang ada disekitarmu</p>
+                </Col>
+                <Col className="justify-content-center" style={{maxWidth:"22rem", maxHeight:"23rem"}}>
+                    <div style={{height:"14rem"}}>
+                        <Image src={EpicStories_Logo}></Image>
+                    </div>
+                    <p className="feature-title">Epic Stories</p>
+                    <p className="feature-text">Temukan cerita sukses yang menginspirasi dari problem pendidikan di indonesia</p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Feature
