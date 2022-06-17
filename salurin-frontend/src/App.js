49b2dfba8d73594eb7843559e2a5aea2bbc6d20d
Zaka Noor <zakanoor@outlook.co.id>
2022-06-17 16:50:15 +0700

    SFP-18 add new router dom

--- salurin-frontend/src/App.js
+++ salurin-frontend/src/App.js
@@ -1,18 +1,49 @@
 import logo from './logo.png';
 import './App.css';
+
+// We use react router dom version 6, so Switch replaced with Routes
+// Import react router dom package
+import {
+  BrowserRouter as Router, 
+  Routes,
+  Route
+} from 'react-router-dom';
+
+// Import component
+// import Home component
+import Home from './Component/Home';
+// Import Navbar component
 import Navbar from './Component/navbar';
-import Footer from './Component/Footer'
+// Import Footer Component
+import Footer from './Component/Footer';
+
+// Import Auth component
+// Import Register component
+import RegisterForm from './Auth/RegisterForm';
+// import Login component
+import LoginForm from './Auth/LoginForm';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <div className="App">
-      <header className="App-header">
+      <Router>
+        <Navbar />
+        <Home />
         
-      </header>
-    </div>
-      <Footer />
+        {/* This is router for navigate to end point */}
+        <Routes>
+          {/* This is home/root folder this app */}
+          <Route path='/' component={<Home/>} /> 
+
+          {/* This is Register route for register page */}
+          <Route path='/register' component={<RegisterForm />} />
+
+          {/* This is Login route for login page */}
+          <Route path='/login' component={<LoginForm />} />
+        </Routes>
+        
+        <Footer />
+      </Router>
     </>
   );
 }
